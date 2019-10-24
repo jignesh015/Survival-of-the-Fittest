22d1efc1d63aa408a682ea921d302789eabc538d
Jignesh Patel <patel.jignesh705@gmail.com>
2019-10-24 23:27:57 +0530

    Added player and camera movement

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    #region "Public variables"
+    #endregion
+
+    #region "Private variables"
+    private Transform lookAt;
+    private Vector3 startOffset;
+    private Vector3 moveVector;
+
+    //For starting camera animation
+    private float transition = 0;
+    private float animationDuration = 2f;
+    private Vector3 animationOffset = new Vector3(0, 4, 0);
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - lookAt.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = lookAt.position + startOffset;
+
+        //Restrict camera movement on X-axis and Y-axis
+        moveVector.x = 0;
+
+        if (transition > 1f)
+        {
+            transform.position = moveVector;
+        }
+        else
+        {
+            //Enable camera animation at the start of the game
+            transform.position = Vector3.Lerp(moveVector + animationOffset, moveVector, transition);
+            transition += Time.deltaTime * 1 / animationDuration;
+            transform.LookAt(lookAt.position + Vector3.up);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    #region "Public variables"
+    public CharacterController characterController;
+    #endregion
+
+    #region "Private variables"
+    private Vector3 moveVector;
+    private float speed = 5f;
+    private float verticalVelocity = 0;
+    private float gravity = 9.8f;
+    private float cameraAnimationDuration = 2f;
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Restrict player interaction during initial camera animation
+        if (Time.time < cameraAnimationDuration)
+        {
+            characterController.Move(Vector3.forward * speed * Time.deltaTime);
+            return;
+        }
+
+
+        moveVector = Vector3.zero;
+
+        //Set X position
+        moveVector.x = Mathf.Clamp(Input.GetAxisRaw("Horizontal") * speed, -2f, 2f);
+
+        //Set Y position
+        verticalVelocity = characterController.isGrounded ? -0.5f : verticalVelocity - (gravity * Time.deltaTime);
+        moveVector.y = verticalVelocity;
+
+        //Set Z position
+        moveVector.z = speed;
+
+        characterController.Move(moveVector * Time.deltaTime);
+    }
+}
